949eb5aa75f3d4aace033262aebbc1f80f17411a
ismailjosim <ismailjosim99@gmail.com>
2025-11-19 12:57:45 +0600

    Fix: Specialties re-render issues when creating new specialty

--- src/components/modules/Admin/SpecialtiesManagement/SpecialtiesFormDialog.tsx
+++ src/components/modules/Admin/SpecialtiesManagement/SpecialtiesFormDialog.tsx
@@ -33,7 +33,7 @@ const SpecialtiesFormDialog = ({
 		} else if (state && !state.success) {
 			toast.error(state.message)
 		}
-	}, [state, onSuccess, onClose])
+	}, [state])
 	return (
 		<Dialog open={open} onOpenChange={onClose}>
 			<DialogContent>
